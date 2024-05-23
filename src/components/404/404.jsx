9553a3eef971bf61ca61b6dd81e0fcc34bc76c09
abrshewube <abrshefru1427@gmail.com>
2024-05-23 12:11:22 -0700

    done

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+
 const NotFound = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6 text-center">404 - Not Found</h1>
-      <p className="text-gray-600 text-center">Sorry, the page you are looking for does not exist.</p>
+      <div className="flex justify-center">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0AJ8GVPW4MWf3A3JsC8akKBW754DIjLSB7Zo53hAj_Q&s" alt="Page not found" className="w-64 h-64" />
+      </div>
+      <p className="text-gray-600 text-center mt-4">Sorry, the page you are looking for does not exist.</p>
     </div>
   );
 };
