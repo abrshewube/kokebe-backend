c366708b777cf4f3492a63a0dd52c9aec027ed52
abrshewube <abrshefru1427@gmail.com>
2024-05-23 14:03:29 -0700

    done

--- src/components/Ui/home/Home.jsx
+++ src/components/Ui/home/Home.jsx
@@ -5,6 +5,7 @@ import img1 from './images/course1.jpg'
 import img2 from './images/course2.jpg';
 import img3 from './images/course3.jpg';
 import av1 from './images/avatar1.jpg'
+import school from './image/school.jpg'
 const Home = () => {
   return (
     <div class='body'>
@@ -29,7 +30,7 @@ const Home = () => {
 
             <div class="header__right">
                 <div class="header__right-image">
-                    <img src="https://cinar00-education-website-html-css-js.netlify.app/images/header.svg" alt=""/>
+                    <img src={school} alt=""/>
                 </div>
             </div>
         </div>
@@ -37,59 +38,56 @@ const Home = () => {
     <br /><br /><br />
       
     <section class="categories">
-        <div class="container categories__container">
-            <div class="categories__left">
-                <h1>Categories</h1>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Architecto praesentium facilis voluptatum illum blanditiis 
-                    distinctio nulla quisquam enim quos fugit accusamus, nesciunt 
-                    doloremque tempora quod quo incidunt sint! Iure, reiciendis!
-                </p>
-                <a href="#" class="btn">Learn More</a>
-            </div>
-
-            <div class="categories__right">
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-bitcoin-circle"></i></span>
-                    <h5>Blockchain</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-                
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-palette"></i></span>
-                    <h5>Graphic Desing</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-                
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-usd-circle"></i></span>
-                    <h5>Finance</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-                
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-megaphone"></i></span>
-                    <h5>Marketing</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-                
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-music"></i></span>
-                    <h5>Music</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-                
-                <article class="category">
-                    <span class="category__icon"><i class="uil uil-puzzle-piece"></i></span>
-                    <h5>Business</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing iure laborum animi.</p>
-                </article>
-            </div>
+    <div class="container categories__container">
+        <div class="categories__left">
+            <h1>School Departments</h1>
+            <p>
+                Explore the various departments at Kokebe Tsibah Highschool, offering diverse fields of study and opportunities for academic growth and exploration.
+            </p>
+            <a href="#" class="btn">Learn More</a>
         </div>
-    </section> 
-        
 
+        <div class="categories__right">
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-bitcoin-circle"></i></span>
+                <h5>Science</h5>
+                <p class="small-char">Delve into the world of scientific inquiry and discovery, exploring biology, chemistry, physics, and more.</p>
+            </article>
+            
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-palette"></i></span>
+                <h5>Arts</h5>
+                <p class="small-char">Cultivate creativity and artistic expression through various mediums including visual arts, music, and performing arts.</p>
+            </article>
+            
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-usd-circle"></i></span>
+                <h5>Finance</h5>
+                <p class="small-char">Gain insights into financial management, investment strategies, and economic principles to prepare for a successful future in finance.</p>
+            </article>
+            
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-megaphone"></i></span>
+                <h5>Communication</h5>
+                <p class="small-char">Develop effective communication skills through courses in journalism, public speaking, media studies, and more.</p>
+            </article>
+            
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-music"></i></span>
+                <h5>Music</h5>
+                <p class="small-char">Explore the art of music through performance, theory, composition, and appreciation of various genres and styles.</p>
+            </article>
+            
+            <article class="category">
+                <span class="category__icon"><i class="uil uil-puzzle-piece"></i></span>
+                <h5>Business</h5>
+                <p class="small-char">Gain practical knowledge and skills in entrepreneurship, management, marketing, and business administration.</p>
+            </article>
+        </div>
+    </div>
+</section>
+
+<br /><br /><br /><br /><br /><br /><br />
 
 
     <section class="courses">
